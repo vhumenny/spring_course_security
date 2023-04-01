c7cab1f341d22fd8abc6a1f263963296c7689ff6
vhumenny <v.humenny@gmail.com>
2023-04-01 20:38:44 +0300

    spring security finished

--- src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.zaurtregulov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,8 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
 import java.util.AbstractCollection;
 import java.util.List;
 
@@ -19,13 +22,17 @@ import static org.springframework.security.core.userdetails.User.*;
 @Configuration
 public class MySecurityConfig {
 
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     public SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request ->
                         request.requestMatchers("/")
-                                .hasAnyRole("EMPLOYEE","HR","MANAGER")
+                                .hasAnyRole("EMPLOYEE", "HR", "MANAGER")
                                 .requestMatchers("/hr_info").hasRole("HR")
-                                .requestMatchers("/manager_info").hasRole("MANAGER").anyRequest().authenticated()
+                                .requestMatchers("/manager_info").hasRole("MANAGER")
+                                .anyRequest().authenticated()
                 ).formLogin()
                 .defaultSuccessUrl("/")
                 .and()
@@ -33,14 +40,15 @@ public class MySecurityConfig {
         return httpSecurity.build();
     }
 
-@Bean
+    @Bean
     protected UserDetailsService userDetailsService() throws Exception {
-
-    return new InMemoryUserDetailsManager((User.withDefaultPasswordEncoder()
-            .username("zaur").password("zaur").roles("EMPLOYEE")
-            .build()), (User.withDefaultPasswordEncoder()
-            .username("vladimir").password("vladimir").roles("HR")
-            .build()), (User.withDefaultPasswordEncoder()
-            .username("anna").password("anna").roles("MANAGER").build()));
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        return users;
+//    return new InMemoryUserDetailsManager((User.withDefaultPasswordEncoder()
+//            .username("zaur").password("zaur").roles("EMPLOYEE")
+//            .build()), (User.withDefaultPasswordEncoder()
+//            .username("vladimir").password("vladimir").roles("HR")
+//            .build()), (User.withDefaultPasswordEncoder()
+//            .username("anna").password("anna").roles("MANAGER").build()));
     }
 }
